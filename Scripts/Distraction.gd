7b0884887f761a82dc3603c6c81187597efdf484
dsmtE <desmet.enguerrand@gmail.com>
2023-02-25 10:13:05 +0100

    Add hover detection

--- Scripts/Distraction.gd
+++ Scripts/Distraction.gd
@@ -1,9 +1,24 @@
 extends Area2D
 
+
+var hovered: bool = false
+
 func _ready():
 	connect("input_event", self, "_on_Area2D_input_event")
+	connect("mouse_entered", self, "_on_Area2D_mouse_entered")
+	connect("mouse_exited", self, "_on_Area2D_mouse_exited")
 
 func _on_Area2D_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	var mouse_event := event as InputEventMouseButton
 	if (mouse_event && mouse_event.pressed && mouse_event.button_index == BUTTON_LEFT):
 		print("%s clicked" % name)
+		
+
+
+func _on_Area2D_mouse_entered():
+	hovered = true
+	print("%s mouse_entered" % name)
+	
+func _on_Area2D_mouse_exited():
+	hovered = false
+	print("%s mouse_exited" % name)
