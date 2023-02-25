c060d587caa1ed9a738dd3cd34b9dd99a1070f97
dsmtE <desmet.enguerrand@gmail.com>
2023-02-25 20:14:10 +0100

    Move static function in Utils class

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -5,7 +5,7 @@ class_name GameManager
 var _distractions: Array = []
 
 func _ready():
-	find_by_class(self, Distraction, _distractions)	
+	Utils.find_by_class(self, Distraction, _distractions)	
 	print(_distractions)
 	
 	for distraction in _distractions:
@@ -14,9 +14,3 @@ func _ready():
 
 func _on_distraction_panic_ended(node):
 	print("Distraction %s ended" % node.name)
-
-static func find_by_class(node: Node, className, result : Array) -> void:
-	if node is className:
-		result.push_back(node)
-	for child in node.get_children():
-		find_by_class(child, className, result)
