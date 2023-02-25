0f9daeac8c79295b5150a3ec0d42395af03beeef
dsmtE <desmet.enguerrand@gmail.com>
2023-02-25 19:57:16 +0100

    Rework Window class using Distraction and Dragable

---
+++ Scripts/Distractions/Window/WindowDistraction.gd
@@ -0,0 +1,4 @@
+extends Distraction
+
+func _on_WindowDragable_target_reached():
+	_end_panic()
---
+++ Scripts/Distractions/Window/WindowDragable.gd
@@ -0,0 +1,41 @@
+extends Dragable
+
+onready var sprite : Sprite = $Sprite
+
+onready var sprite_parent: Node2D = sprite.get_parent()
+
+var _start_dragging_local_offset: Vector2 = Vector2()
+
+func _ready():
+	connect("dragging_started", self, "_dragging_started")
+
+func tween_to_starting_state(starting_pos: Vector2, current_pos: Vector2):
+	tween.interpolate_property(
+			sprite,
+			"position",
+			null,
+			Vector2(),
+			0.2,
+			Tween.EASE_IN,
+			Tween.EASE_IN
+		)
+	tween.start()
+	
+func tween_to_current_state(starting_mouse_pos: Vector2, current_mouse_pos: Vector2):
+	
+	var local_mouse_pos: Vector2 = sprite_parent.to_local(current_mouse_pos)
+	var target_position: Vector2 = local_mouse_pos + _start_dragging_local_offset
+	
+	tween.interpolate_property(
+			sprite,
+			"position",
+			null,
+			Vector2(sprite.position.x, target_position.y),
+			0.1,
+			Tween.EASE_IN
+		)
+	tween.start()
+
+func dragging_started(mouse_pos: Vector2):
+	var local_mouse_pos: Vector2 = sprite_parent.to_local(mouse_pos)
+	_start_dragging_local_offset = sprite.position-local_mouse_pos
